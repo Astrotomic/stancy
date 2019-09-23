e2e2a82a7b564646368e87c847585f7710a1ff3f
Gummibeer <dev.gummibeer@gmail.com>
2019-09-23 18:20:00 +0200

    do not force page data property

--- src/Models/PageData.php
+++ src/Models/PageData.php
@@ -3,14 +3,10 @@
 namespace Astroromic\Stancy\Models;
 
 use Illuminate\Contracts\Support\Arrayable;
-use Illuminate\Support\HtmlString;
 use Spatie\DataTransferObject\DataTransferObject;
 
 abstract class PageData extends DataTransferObject implements Arrayable
 {
-    /** @var HtmlString */
-    public $contents;
-
     public static function make(array $data): self
     {
         return new static($data);
