a80c8f8cc3c20fc8960079a6a960d546faab41f5
Gummibeer <dev.gummibeer@gmail.com>
2019-10-07 16:24:33 +0200

    fix page factory facade doc-tag

--- src/Facades/PageFactory.php
+++ src/Facades/PageFactory.php
@@ -5,11 +5,13 @@ namespace Astrotomic\Stancy\Facades;
 use Astrotomic\Stancy\Contracts\Page as PageContract;
 use Astrotomic\Stancy\Contracts\PageFactory as PageFactoryContract;
 use Illuminate\Support\Facades\Facade;
+use Spatie\Sheets\Sheet;
 
 /**
  * @method static PageContract make(array $data = [], ?string $page = null)
- * @method static PageContract makeFromSheet(string $collection, string $name, ?string $page = null)
+ * @method static PageContract makeFromSheet(Sheet $sheet, ?string $page = null)
  * @method static PageContract makeFromSheetName(string $collection, string $name, ?string $page = null)
+ *
  * @see PageFactoryContract
  */
 class PageFactory extends Facade
